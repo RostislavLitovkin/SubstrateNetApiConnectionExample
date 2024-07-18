aa2075496869edbb51f2d2ed30189083f8ad071d
Rostislav Litovkin <rostislavlitovkin@gmail.com>
2024-07-18 14:35:37 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Substrate.NetApi;
+
+// Change to wss://127.0.0.1:8000
+var wss = "wss://rpc.helikon.io/hydradx";
+
+Console.WriteLine("Connecting to: " + wss);
+
+var client = new SubstrateClient(new Uri(wss), default);
+
+await client.ConnectAsync();
+
+Console.WriteLine("Successfully connected to: " + await client.System.ChainAsync());
+
+Console.ReadKey();
